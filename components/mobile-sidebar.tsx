c7e8df92fbcf00f0e7bf52da811b51cb54de6a62
WinnardArthur <winnardarthur7@gmail.com>
2024-01-09 10:39:20 +0000

    improve sidebar

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -1,11 +1,22 @@
-import React from "react";
+'use client';
+
+import React, { useEffect, useState } from "react";
 
 import { Menu } from "lucide-react";
+
 import { Button } from "@/components/ui/button";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import Sidebar from "@/components/sidebar";
 
 const MobileSidebar = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) return null;
+
   return (
     <Sheet>
       <SheetTrigger>
@@ -13,7 +24,7 @@ const MobileSidebar = () => {
           <Menu />
         </Button>
       </SheetTrigger>
-      <SheetContent side='left' className="p-0">
+      <SheetContent side="left" className="p-0">
         <Sidebar />
       </SheetContent>
     </Sheet>

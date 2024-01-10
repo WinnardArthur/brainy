9fd4760d32940cdcf7dd92b095a5654daa77cffe
WinnardArthur <winnardarthur7@gmail.com>
2024-01-10 22:11:36 +0000

    create image generation

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains: [
+            'oaidallepiprodscus.blob.core.windows.net',
+        ]
+    }
+}
 
 module.exports = nextConfig

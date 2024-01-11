769f443afbd50bde8e755e329a48d66c9ed6456d
WinnardArthur <winnardarthur7@gmail.com>
2024-01-11 16:41:15 +0000

    create upgrade modal

--- components/FreeCounter.tsx
+++ components/FreeCounter.tsx
@@ -5,12 +5,14 @@ import { Card, CardContent } from "@/components/ui/card";
 import { MAX_FREE_COUNTS } from "@/constants";
 import { Progress } from "@/components/ui/progress";
 import { Button } from "@/components/ui/button";
+import { useProModal } from "@/hooks/use-pro-modal";
 
 type FreeCounterProps = {
   apiLimitCount: number;
 };
 
 const FreeCounter = ({ apiLimitCount = 0 }: FreeCounterProps) => {
+  const proModal = useProModal();
   const [mounted, setMounted] = useState(false);
 
   useEffect(() => {
@@ -32,7 +34,11 @@ const FreeCounter = ({ apiLimitCount = 0 }: FreeCounterProps) => {
               value={(apiLimitCount / MAX_FREE_COUNTS) * 100}
             />
           </div>
-          <Button className="w-full" variant='premium'>
+          <Button
+            onClick={() => proModal.onOpen}
+            className="w-full"
+            variant="premium"
+          >
             Upgrade
             <Zap className="w-4 h-4 ml-2 fill-white" />
           </Button>

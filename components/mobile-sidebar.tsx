4684dc0d7386dbd12c61c2d3c36ba7f6ded8aa96
WinnardArthur <winnardarthur7@gmail.com>
2024-01-11 16:59:20 +0000

    add api limit count to mobile sidebar

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import React, { useEffect, useState } from "react";
 
@@ -8,7 +8,11 @@ import { Button } from "@/components/ui/button";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import Sidebar from "@/components/sidebar";
 
-const MobileSidebar = () => {
+type MobileSidebarProps = {
+  apiLimitCount: number;
+};
+
+const MobileSidebar = ({ apiLimitCount = 0 }: MobileSidebarProps) => {
   const [isMounted, setIsMounted] = useState(false);
 
   useEffect(() => {
@@ -25,7 +29,7 @@ const MobileSidebar = () => {
         </Button>
       </SheetTrigger>
       <SheetContent side="left" className="p-0">
-        <Sidebar />
+        <Sidebar apiLimitCount={apiLimitCount} />
       </SheetContent>
     </Sheet>
   );
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { UserButton } from "@clerk/nextjs";
 
-import { Button } from "@/components/ui/button";
 import MobileSidebar from "@/components/mobile-sidebar";
+import { getApiLimitCount } from "@/lib/api-limit";
 
-const Navbar = () => {
+const Navbar = async () => {
+  const apiLimitCount = await getApiLimitCount();
+
   return (
     <div className="flex items-center p-4">
-      <MobileSidebar />
+      <MobileSidebar apiLimitCount={apiLimitCount}/>
       <div className="flex w-full justify-end">
         <UserButton afterSignOutUrl="/" />
       </div>

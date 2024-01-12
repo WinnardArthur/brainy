11868b12444126d026df142f48b28f4ead491ad2
WinnardArthur <winnardarthur7@gmail.com>
2024-01-12 09:14:11 +0000

    add stripe payment and webhook

--- components/pro-modal.tsx
+++ components/pro-modal.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import {
-  ArrowRight,
   Check,
   Code,
   ImageIcon,
@@ -66,7 +66,24 @@ const tools = [
 ];
 
 export const ProModal = () => {
+  const [loading, setLoadinig] = useState(false);
+
   const proModal = useProModal();
+
+  const onSubscribe = async () => {
+    try {
+      setLoadinig(true);
+
+      const response = await axios.get("/api/stripe");
+
+      window.location.href = response.data.url;
+    } catch (error) {
+      console.log(error, "STRIPE_CLIENT_ERROR");
+    } finally {
+      setLoadinig(false);
+    }
+  };
+
   return (
     <Dialog open={proModal.isOpen} onOpenChange={proModal.onClose}>
       <DialogContent>
@@ -98,7 +115,12 @@ export const ProModal = () => {
           </DialogDescription>
         </DialogHeader>
         <DialogFooter>
-          <Button size="lg" variant="premium" className="w-full">
+          <Button
+            size="lg"
+            variant="premium"
+            className="w-full"
+            onClick={onSubscribe}
+          >
             Upgrade
             <Zap className="w-4 h-4 ml-2 fill-white" />
           </Button>
